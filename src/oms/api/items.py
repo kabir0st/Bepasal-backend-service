2095f1e0897395b599a781a3c4f836f4e7d9bcac
Kabir S. Tamari <kabirtamari@gmail.com>
2023-11-15 18:41:59 +0545

    updated api for oms;

--- src/oms/api/items.py
+++ src/oms/api/items.py
@@ -1,23 +1,19 @@
 from core.permissions import IsAdminOrReadOnly
 from core.utils.viewsets import DefaultViewSet
-from oms.api.serializers.item import (ItemListSerializer, ItemSerializer,
+from oms.api.serializers.item import (CategorySerializer, ItemImageSerializer,
+                                      ItemListSerializer, ItemSerializer,
                                       ItemVariationImageSerializer,
                                       ItemVariationSerializer,
                                       VariationTypeSerializer)
-from oms.models.item import (Item, ItemVariation, ItemVariationImage,
-                             VariationType)
+from oms.models.item import (Category, Item, ItemImage, ItemVariation,
+                             ItemVariationImage, VariationType)
 
 
 class CategoryAPI(DefaultViewSet):
-    serializer_class = ItemSerializer
+    serializer_class = CategorySerializer
     search_fields = ["name"]
     permission_classes = [IsAdminOrReadOnly]
-    queryset = Item.objects.filter().order_by('-id')
-
-    def get_serializer_class(self):
-        if self.action == 'list':
-            return ItemListSerializer
-        return super().get_serializer_class()
+    queryset = Category.objects.filter().order_by('-id')
 
 
 class ItemAPI(DefaultViewSet):
@@ -34,15 +30,9 @@ class ItemAPI(DefaultViewSet):
 
 
 class ItemImageAPI(DefaultViewSet):
-    serializer_class = ItemSerializer
+    serializer_class = ItemImageSerializer
     permission_classes = [IsAdminOrReadOnly]
-    queryset = Item.objects.filter().order_by('-id')
-
-    def get_queryset(self):
-        slug = self.kwargs.get('item_slug', None)
-        if slug is None:
-            return self.queryset.none()
-        return self.queryset.filter(item__slug=slug)
+    queryset = ItemImage.objects.filter().order_by('-id')
 
 
 class VariationTypeAPI(DefaultViewSet):
